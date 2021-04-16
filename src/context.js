878905c40cf7d8dfa86bccfdada34fbb7a6bff00
Abdurrohim_ethan <abdurrohim48@gmail.com>
2021-04-16 10:17:00 +0700

    importing AppProvider context to index.js

--- src/context.js
+++ src/context.js
@@ -1,20 +1,36 @@
 import React, { useState, useContext } from "react";
 import sublinks from "./data";
 const AppContext = React.createContext();
-const AppProvider = ({ children }) => {
+export const AppProvider = ({ children }) => {
   const [isSideBarOpen, setIsSideBarOpen] = useState(true);
-  const [isModalOpen, setIsModalOpen] = useState(true);
+  const [isSubMenuOpen, setIsSubMenuOpen] = useState(true);
   const openSideBar = () => {
     setIsSideBarOpen(true);
   };
   const closeSideBar = () => {
     setIsSideBarOpen(false);
   };
-  const openModal = () => {
-    setIsModalOpen(true);
+  const openSubMenu = () => {
+    setIsSubMenuOpen(true);
   };
-  const closeModal = () => {
-    setIsModalOpen(false);
+  const closeSubMenu = () => {
+    setIsSubMenuOpen(false);
   };
-  return <AppContext.Provider>{children}</AppContext.Provider>;
+  return (
+    <AppContext.Provider
+      value={{
+        isSubMenuOpen,
+        isSideBarOpen,
+        openSideBar,
+        closeSideBar,
+        openSubMenu,
+        closeSubMenu,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+export const useGlobalContext = () => {
+  return useContext(AppContext);
 };
