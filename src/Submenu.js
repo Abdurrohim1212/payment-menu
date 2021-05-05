065ce8c06287da8054ada4b7b66b996d61f817df
Abdurrohim_ethan <abdurrohim48@gmail.com>
2021-05-05 14:06:29 +0700

    adding useEffect when location change and get specific coordinates

--- src/Submenu.js
+++ src/Submenu.js
@@ -3,8 +3,18 @@ import { useGlobalContext } from "./context";
 
 const Submenu = () => {
   const { isSubMenuOpen, location } = useGlobalContext();
+  const container = useRef(null);
+  useEffect(() => {
+    const submenu = container.current;
+    const { center, bottom } = location;
+    submenu.style.left = `${center}px`;
+    submenu.style.top = `${bottom}px`;
+  }, [location]);
   return (
-    <aside className={`${isSubMenuOpen ? `submenu show` : `submenu`}`}>
+    <aside
+      className={`${isSubMenuOpen ? `submenu show` : `submenu`}`}
+      ref={container}
+    >
       ethan kurniawan
     </aside>
   );
